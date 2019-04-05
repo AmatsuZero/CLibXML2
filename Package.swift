92b0074453fd5db34fa9a6585cce17ab6b16f52c
Mike <mdaxter@users.noreply.github.com>
2019-04-05 19:48:18 +1000

    Update to version 4 package format

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
+// swift-tools-version:4.0
+
 import PackageDescription 
 
 let package = Package(
 	name: "CLibXML2",
 	pkgConfig: "libxml-2.0",
 	providers: [
-		.Brew("libxml2"),
-		.Apt("libxml2-dev")
+		.brew(["libxml2"]),
+		.apt(["libxml2-dev"])
 	]
 )
